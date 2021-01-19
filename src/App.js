3b4759073b09d0b43d564cf57be030bf604163b8
Aleksandar Aleksic <aleksica@gmail.com>
2021-01-19 10:46:01 +0100

    Kraj Lekcije 2 sa NJS 14 i Node-Sass 4.14.1, sa NVM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return <div className='App'>Hello World</div>;
-  }
+import HomePage from './homepage.components';
+
+function App() {
+  return <div>
+    <HomePage />
+  </div>
 }
 
 export default App;
