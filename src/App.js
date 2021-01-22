062ab8362c77490d1aebc588f2f5368d7d37e24a
Aleksandar Aleksic <aleksica@gmail.com>
2021-01-22 16:15:43 +0100

    Kraj lekcije 101

--- src/App.js
+++ src/App.js
@@ -7,27 +7,38 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component.jsx";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
-  constructor () {
+  constructor() {
     super();
-  
+
     this.state = {
-      currentUser: null
-    }
+      currentUser: null,
+    };
   }
 
   unsubcribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubcribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
-    })
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }
+      else {this.setState({ currentUser: userAuth });}
+       // this should be wrapped in an "else" so this.setState() does not fire twice.
+    });
   }
-  
+
   componentWillUnmount() {
     this.unsubcribeFromAuth();
   }
@@ -35,7 +46,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -24,7 +24,7 @@ const Header = ( {currentUser} ) => (
         <div className="option" onClick={() => auth.signOut()}>
         SIGN OUT</div>
         :
-        <Link classNamee="option" to='/signin'>SIGN IN</Link>
+        <Link className="option" to='/signin'>SIGN IN</Link>
       }
     </div>
   </div>
